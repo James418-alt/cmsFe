7e0a15537893cd766fe9bbe1daef33e9b586ee6c
James418-alt <ebifegha123@gmail.com>
2024-07-13 12:53:56 +0100

    all done

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -12,19 +12,30 @@ const Hero = async () => {
     <div>
       <div className="md:px-[150px] md:pt-[55px] mb-6 px-[20px] py-[20px]">
         <h1
-          className={`font-bold text-[30px] text-[${data?.data[0]?.headercolor}] w-[250px] leading-10  md:text-[60px] font-sans md:leading-[70px] pb-6 md:w-[500px]`}
+          style={{ color: `${data?.data[0]?.headercolor}` }}
+          className={`font-bold text-[30px] w-[250px] leading-10  md:text-[60px] font-sans md:leading-[70px] pb-6 md:w-[500px]`}
         >
           {data?.data[0]?.title}
         </h1>
-        <p className={`font-medium text-[14px] text-[] mb-8 w-[300px]`}>
+        <p
+          style={{ color: `${data?.data[0]?.desccolor}` }}
+          className={`font-medium text-[14px]  mb-8 w-[300px]`}
+        >
           {data?.data[0]?.desc}
         </p>
         <div className="flex gap-6 items-center">
-          <button className="p-2 rounded-md bg-blue-700 font-semibold text-[14px] text-white">
+          <button
+            style={{ backgroundColor: `${data?.data[0]?.btncolor}` }}
+            className="p-2 rounded-md bg-blue-700 font-semibold text-[14px] text-white"
+          >
             {data?.data[0]?.btnText}
           </button>
           <div className="flex gap-2 items-center">
-            <Link className="underline font-semibold text-[14px]" href={""}>
+            <Link
+              style={{ color: `${data?.data[0]?.linkcolor}` }}
+              className="underline font-semibold text-[14px]"
+              href={""}
+            >
               {data?.data[0]?.linkText}
             </Link>
             <Image src={"/witheArrow.svg"} alt="#" width={10} height={10} />
