68d411f264af54012614c1eaad2feff7782fdeec
James418-alt <ebifegha123@gmail.com>
2024-07-14 10:54:02 +0100

    new commit today

--- app/Head.tsx
+++ app/Head.tsx
@@ -1,50 +1,40 @@
-"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 import { MdMenu } from "react-icons/md";
 
-const Header = () => {
-  const navs = [
-    { id: 1, title: "Products" },
-    { id: 2, title: "Customers" },
-    { id: 3, title: "Company" },
-    { id: 4, title: "Pricing" },
-  ];
-  const [show, setShow] = useState(false);
+const Header = async () => {
+  const url = "http://localhost:3000/api/header";
+  const res = await fetch(url, { method: "GET", cache: "no-cache" });
+  const data = await res.json();
+  // console.log(data.data[0].navs);
+
   return (
     <div>
       <div>
         <div className="flex justify-between items-center px-[30px] md:px-[150px] py-7">
           <div className="flex gap-10">
             <div>
-              <Image src={"/blackLogo.svg"} alt="#" width={100} height={100} />
+              <h1 className="font-bold text-[15px]">{data.data[0].logo}</h1>
             </div>
             <div className=" hidden md:flex gap-8">
-              {navs.map((el: any) => (
-                <div key={el.id}>
-                  <h2 className="text-gray-500 text-[15px]">{el.title}</h2>
+              {data.data[0].navs.map((el: any, i: any) => (
+                <div key={i}>
+                  <h2 className="text-gray-500 text-[15px]">{el}</h2>
                 </div>
               ))}
             </div>
           </div>
-          <div className="hidden md:flex gap-3">
-            <div className="bg-gray-300 p-2 rounded-md">
-              <Image src={"/phone.svg"} alt="#" width={15} height={15} />
-            </div>
-            <div className="bg-blue-800 flex justify-center items-center gap-[0.9] text-white font-semibold p-2 rounded-md h-8 text-[12px]">
-              <h1>My</h1>
-              <Image src={"/witheLogo.svg"} alt="#" width={50} height={50} />
-              <h1>Cloud</h1>
+          <div className="hidden md:flex">
+            <div
+              style={{ backgroundColor: `${data.data[0].btnColor}` }}
+              className="flex justify-center items-center gap-[0.9] text-white font-semibold px-4 py-2 rounded-md h-8 text-[12px]"
+            >
+              {data.data[0].btnText}
             </div>
           </div>
 
-          <div
-            className="md:hidden"
-            onClick={() => {
-              setShow(!show);
-            }}
-          >
+          <div className="md:hidden">
             <MdMenu />
           </div>
         </div>
@@ -52,24 +42,6 @@ const Header = () => {
           <div className="border-b w-[76%]" />
         </div>
       </div>
-      {show ? (
-        <div className=" md:hidden flex flex-col gap-2 items-end justify-center px-[30px]">
-          {navs.map((el: any) => (
-            <div
-              onClick={() => {
-                setShow(false);
-              }}
-              key={el.id}
-            >
-              <h2 className="text-gray-500 text-[15px] font-semibold">
-                {el.title}
-              </h2>
-            </div>
-          ))}
-        </div>
-      ) : (
-        ""
-      )}
     </div>
   );
 };
